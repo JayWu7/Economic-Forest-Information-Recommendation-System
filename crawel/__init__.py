a31807396b3587a52d943c55efb6f3e7b8e22cd9
jaywu <jaywu16@163.com>
2019-01-31 20:00:27 +0800

    write crawel scripts

--- crawel/__init__.py
+++ crawel/__init__.py
@@ -1,1 +1,7 @@
-from .config import SpiderConfig
+from urllib import request
+
+
+def get_request(config):
+    return request.Request(config.host_url, headers=config.header)
+
+
--- crawel/spider.py
+++
@@ -1,5 +0,0 @@
-from urllib import request
-
-def crawel_tender(url):   #crawel the tender information
-    page = request.urlopen(url)
-    page.read()
