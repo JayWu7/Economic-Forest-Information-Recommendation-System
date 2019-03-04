926869de1e2260d8feb965463d56433376bc3920
jaywu <jaywu16@163.com>
2019-03-04 22:29:45 +0800

    create itao_supply spider

--- crawls/crawls/spiders/itao_spider.py
+++ crawls/crawls/spiders/itao_spider.py
@@ -1,7 +1,8 @@
 from scrapy.spiders import Spider
-from scrapy import FormRequest,Request
+from scrapy import FormRequest, Request
 from ..items import CrawelsDemandItem
 import json
+
 
 class PurchaseDemand(Spider):
     name = 'itao_purchase_demands'
@@ -9,12 +10,11 @@ class PurchaseDemand(Spider):
 
     def start_requests(self):
         url = 'http://www.itaomiao.com/purchases/getIndexPurchaseList.json'
-        for page in range(1,51):
-            form_data = {'page':str(page),'rows':'15'}
-            yield FormRequest(url=url,formdata=form_data,callback=self.parse_json)
+        for page in range(1, 51):
+            form_data = {'page': str(page), 'rows': '15'}
+            yield FormRequest(url=url, formdata=form_data, callback=self.parse_json)
 
-
-    def parse_json(self,response):
+    def parse_json(self, response):
         json_str = json.loads(response.body.decode('utf-8'))
         for info in json_str['indexPurchaseList']:
             item = CrawelsDemandItem()
@@ -32,3 +32,25 @@ class PurchaseDemand(Spider):
             yield item
 
 
+class SupplyCrawl(Spider):
+    name = 'itao_supply'
+    allowed_domains = ['itaomiao.com']
+
+    def start_requests(self):
+        url = 'http://www.itaomiao.com/supply/findSupplyList.json'
+        for i in range(1, 2):
+            form_data = {'pageNo': str(i), 'rows': '20'}
+            yield FormRequest(url=url, formdata=form_data, callback=self.parse_page)
+
+    def parse_page(self, Response):
+        supply_list = json.loads(Response.text)
+        if 'supplyList' in supply_list.keys():
+            for plant in supply_list.get('supplyList'):
+                if 'barcode2D' in plant.keys():
+                    yield Request(
+                        url='http://www.itaomiao.com/supply/supplyInfo.itm?code={code}'.format(code = plant.get('barcode2D')),
+                        callback=self.parse_plant)
+
+    def parse_plant(self, Response):
+        #todo  create item
+        print(Response.text)
