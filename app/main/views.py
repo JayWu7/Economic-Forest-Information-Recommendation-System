8b1e5983daf3e2e744c2d83c5e17752e6146e660
cloudin <cloudin@JayWu-MacBook-Pro.local>
2019-04-16 19:05:24 +0800

    finish the page design of index.html

--- app/main/views.py
+++ app/main/views.py
@@ -3,5 +3,4 @@ from . import main
 
 @main.route('/')
 def index():
-    # return render_template('main/index.html')
-    return render_template('base.html')
+    return render_template('main/index.html')
